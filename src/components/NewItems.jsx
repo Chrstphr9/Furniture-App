29c624fdebdd28a6ca868150822271a71efd3251
somto <somto565@gmail.com>
2025-02-22 12:44:26 +0100

    `Refactor NewItems component to use newInStore data and remove NewItemsSlider component`

--- src/components/NewItems.jsx
+++ src/components/NewItems.jsx
@@ -1,7 +1,26 @@
 import React from 'react';
+import {newInStore} from '../data'
 
 const NewItems = () => {
-  return <div>NewItems</div>;
+  // destructure newInStore
+  const {title, subtitle, link, icon} = newInStore;
+
+  return (
+  <section className='section'>
+      <div className="container mx-auto">
+        <div>
+            <div>
+              <h2>{title}</h2>
+              <p>{subtitle}</p>
+              <div>
+                <a href="#">{link}</a>
+              </div>
+            </div>
+            <NewItemsSlider />
+      </div>
+      </div>
+  </section>
+  )
 };
 
 export default NewItems;
